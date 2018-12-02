48ab3236b9b99fcb0694c5f9c08517b05aea8a8a
PhHitachi <44565442+PhHitachi@users.noreply.github.com>
2018-12-02 22:33:10 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,3 +1,3 @@
 git clone --depth=1 https://github.com/PhHitachi/SecurityXploit.git
-sudo chmod +x xploit/install.sh
-bash xploit/install.sh
+sudo chmod +x SecurityXploit/install.sh
+bash SecurityXploit/install.sh
