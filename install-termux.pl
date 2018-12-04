4500ce035acd5c6276ecbc6c5be2e9f1999df55c
PhHitachi <44565442+PhHitachi@users.noreply.github.com>
2018-12-05 01:46:32 +0800

    Update install-termux.pl

--- install-termux.pl
+++ install-termux.pl
@@ -1,5 +1,4 @@
 use if $^O eq "MSWin32", Win32::Console::ANSI;
 if ($^O =~ /MSWin32/) {system("cls"); }else { system("clear"); }
-system("chmod +x SecurityXploit/termux.sh");
-system("bash SecurityXploit/termux.sh");
-system("perl Xploit.pl");
+system("chmod +x termux.sh");
+system("bash termux.sh");
