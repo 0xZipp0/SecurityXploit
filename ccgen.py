0931374a0820246902358e223ad83af0588b20ff
PhHitachi <44565442+PhHitachi@users.noreply.github.com>
2018-12-07 17:35:27 +0800

    Add files via upload

---
+++ ccgen.py
@@ -0,0 +1,20 @@
+import sys, random ,time, os
+
+time.sleep(1)
+def generator():
+    bin = raw_input("Enter Bin (Ex :483745) : ")
+    m =  raw_input("Month (Ex :01)   : ")
+    y =  raw_input("Year  (Ex :2018) : ")
+    i=1 
+	
+    for i in range(0,200): #range of generate
+            i=i+1
+	    code = random.randint(100,999) #Security Code
+	    number = random.randint(1000000000,v) # Limit of CreditCard Number
+	    creditcard = (bin,number)
+	    cc = "%s%s"%(creditcard)
+	    all = cc,"|",m,"|",y,"|",code
+	    allstring = "%s%s%s%s%s%s%s"%(all)
+	    print allstring
+
+generator()
