1b98a5a293379060ae8f4132f8acdeb36a0901a7
PhHitachi <44565442+PhHitachi@users.noreply.github.com>
2018-12-10 03:32:34 +0800

    Update termux.sh

--- termux.sh
+++ termux.sh
@@ -23,6 +23,4 @@ cpan install Term::ANSIColor
 cpan install URI::URL
 cpan install IO::Socket::INET
 cpan install Try::Tiny
-echo "[✔]====================================================================[✔]";
-echo "[✔]  Tool installed successfully! \nYou can execute SecurityXploit now [✔]";
-echo "[✔]====================================================================[✔]";
+echo "[✔] All is done! [✔]";
