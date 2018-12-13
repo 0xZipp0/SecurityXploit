614e2a51d01c60839812111140869b9b9d021ee9
PhHitachi <44565442+PhHitachi@users.noreply.github.com>
2018-12-13 18:37:35 +0800

    Update termux.sh

--- termux.sh
+++ termux.sh
@@ -13,14 +13,3 @@ perl Makefile.PL
 make
 make test
 make install
-cpan install Getopt::Long
-cpan install HTTP::Request
-cpan install LWP::UserAgent
-cpan install IO::Select
-cpan install HTTP::Cookies
-cpan install HTTP::Response
-cpan install Term::ANSIColor
-cpan install URI::URL
-cpan install IO::Socket::INET
-cpan install Try::Tiny
-echo "[✔] All is done! [✔]";
