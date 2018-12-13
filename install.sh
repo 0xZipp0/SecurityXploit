9edf184099ce8188a05b374030a02ae906500598
PhHitachi <44565442+PhHitachi@users.noreply.github.com>
2018-12-13 18:38:59 +0800

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,13 @@
+cpan install Getopt::Long
+cpan install HTTP::Request
+cpan install LWP::UserAgent
+cpan install IO::Select
+cpan install HTTP::Cookies
+cpan install HTTP::Response
+cpan install Term::ANSIColor
+cpan install URI::URL
+cpan install IO::Socket::INET
+cpan install Try::Tiny
+echo "[✔]====================================================================[✔]";
+echo "[✔]  Tool installed successfully! \nYou can execute SecurityXploit now [✔]";
+echo "[✔]====================================================================[✔]";
